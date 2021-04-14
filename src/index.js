f6ba9befe0e24901d7850a94ec556da89a721bca
ture23 <47562456+ture23@users.noreply.github.com>
2021-04-14 20:23:22 +0200

    Added Styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-export default function index() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import App from './App'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
